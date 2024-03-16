ee46093915a719da85f0f58fc1783e1278556eb1
Sayan Das <sayandasraja56@gmail.com>
2024-03-17 03:42:39 +0530

    commit

---
+++ 01Vite_react/src/App.jsx
@@ -0,0 +1,10 @@
+
+function App() {
+
+
+  return (
+    <h1>second one</h1>
+  )
+}
+
+export default App
---
+++ 01basics/src/App.js
@@ -0,0 +1,9 @@
+
+
+function App() {
+  return (
+    <h1>first one brooo</h1>
+  );
+}
+
+export default App;
